15ee9c60d726298d08c9549fbb803a60db82fb3e
Christopher Pelletier <chris.r.pelletier@gmail.com>
2017-05-14 20:28:49 -0700

    Added an about component for the about view

---
+++ src/components/About/About.js
@@ -0,0 +1,13 @@
+import Component from 'inferno-component';
+
+class About extends Component {
+  render(props, state) {
+    return(
+      <div className="App-content">
+        <h4>About Page</h4>
+      </div>
+    );
+  }
+}
+
+export default About;
