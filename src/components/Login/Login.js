e297125a6470be69657db2485f1b4a79b66f3cae
Christopher Pelletier <chris.r.pelletier@gmail.com>
2017-05-14 20:30:13 -0700

    Removed a console log that is no longer needed.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,10 +3,6 @@ import Component from 'inferno-component';
 import AuthService from './../../utils/AuthService'
 
 class Login extends Component {
-	componentDidMount() {
-		console.log(this);
-	}
-
 	render() {
 		return(
 		    <a onClick={linkEvent(this, AuthService.login)}>Log In</a>
