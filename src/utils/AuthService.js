a3c0673fb9a32199f76be7b0587730bd053e85e9
Christopher Pelletier <chris.r.pelletier@gmail.com>
2017-05-14 20:31:21 -0700

    Added check for checking is a user is logged in.

--- src/utils/AuthService.js
+++ src/utils/AuthService.js
@@ -2,11 +2,22 @@
 
 import auth0 from 'auth0-js';
 
+const AuthService = { 
+	login, 
+	logOut, 
+	isLoggedIn,
+	createWebAuthInstance, 
+	parseHash, 
+	getIdToken,
+	getAccessToken,
+	getProfile
+};
+
 function login(instance) {
-	console.log(instance);
+	let redirectUri = 'http://localhost:3000' + instance.context.router.location.pathname;
 	instance.props.auth0.authorize({
 		responseType: 'token id_token',
-		redirectUri: 'http://localhost:3000/',
+		redirectUri: redirectUri,
 		audience: 'http://localhost:3001/api/',
 		scope: 'openid profile'
 	});
@@ -21,6 +32,11 @@ function logOut(instance) {
 	localStorage.removeItem('id_token');
 	localStorage.removeItem('access_token');
 	localStorage.removeItem('profile');
+	instance.context.router.push('/');
+}
+
+function isLoggedIn() {
+	return getIdToken() && getAccessToken() && getProfile();
 }
 
 function createWebAuthInstance() {
@@ -35,18 +51,30 @@ function parseHash(instance) {
 		if(authResult && authResult.idToken && authResult.accessToken) {
 			// Retrieve the users profile.
 			instance.auth0.client.userInfo(authResult.accessToken, (err, profile) => {
+				window.location = window.location.href.split('#')[0];
+
 				instance.setState({
 					idToken: authResult.idToken,
 					profile: profile
 				});
-
 				localStorage.setItem('id_token', instance.state.idToken);
-				localStorage.setItem('access_token', instance.state.accessToken);
+				localStorage.setItem('access_token', authResult.accessToken);
 				localStorage.setItem('profile', JSON.stringify(profile));
 			}); 
 		}
 	});
 }
 
-const AuthService = { login, logOut, createWebAuthInstance, parseHash };
+function getIdToken() {
+	return localStorage.getItem('id_token');
+}
+
+function getAccessToken() {
+	return localStorage.getItem('access_token');
+}
+
+function getProfile() {
+	return JSON.parse(localStorage.getItem('profile'));
+}
+
 export default AuthService;
