4dea8f9545390840f2a287132648489eab7699ba
Christopher Pelletier <chris.r.pelletier@gmail.com>
2017-05-20 20:24:39 -0700

    Converted the layout component to a component class instead of a function.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,19 @@
+import Component from 'inferno-component';
 import NavBar from './../NavBar/NavBar';
 
-export default function({ children }) {
-    return <div>
-        <NavBar/>
-        <main>
-            <div className="App-content">
-                {children}
+class Layout extends Component {
+    render(props, state) {
+        return (
+            <div>
+                <NavBar/>
+                <main>
+                    <div className="App-content">
+                        {props.children}
+                    </div>
+                </main>
             </div>
-        </main>
-    </div>
+        );
+    }
 }
+
+export default Layout;
