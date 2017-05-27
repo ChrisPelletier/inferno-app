71688e284a23a0e7e45397167151ed70575d0b21
Christopher Pelletier <chris.r.pelletier@gmail.com>
2017-05-27 10:49:47 -0700

    Set the default authorization header for all requests.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Router } from 'inferno-router';
 import Component from 'inferno-component';
 import axios from 'axios';
+import AuthService from './utils/AuthService';
 import routes from './routes';
 import './App.css';
 
 axios.defaults.baseURL = 'http://localhost:3001/api';
+axios.defaults.headers.common['Authorization'] = 
+					'Bearer ' + AuthService.getAccessToken();
 
 class App extends Component {
   render({ history }) {
